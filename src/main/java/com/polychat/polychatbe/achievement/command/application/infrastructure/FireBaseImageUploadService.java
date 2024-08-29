d748dc7c8f69480abe10770c9e3e1233857e56b3
gsh118 <hks01108@naver.com>
2024-08-29 09:37:52 +0900

    #13 업적 목록 엔티티 추가
    업적 목록 엔티티 정의, crud 서비스 코드 추가

---
+++ src/main/java/com/polychat/polychatbe/achievement/command/application/infrastructure/FireBaseImageUploadService.java
@@ -0,0 +1,25 @@
+package com.polychat.polychatbe.achievement.command.application.infrastructure;
+
+import com.polychat.polychatbe.achievement.command.domain.service.ImageUploadService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+// 아직 미구현
+
+@Service
+public class FireBaseImageUploadService implements ImageUploadService {
+    @Override
+    public String getFileUrl(String fileName) {
+        return "";
+    }
+
+    @Override
+    public String uploadImage(String fileName, MultipartFile file) {
+        return "";
+    }
+
+    @Override
+    public String deleteImage(String fileName) {
+        return "";
+    }
+}
