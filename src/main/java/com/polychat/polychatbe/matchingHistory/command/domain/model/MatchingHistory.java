8727c86c20ebe35e89f0a111efa76ed93e356163
GarlGaru <juon304@gmail.com>
2024-08-30 18:34:42 +0900

    crud 작성중

--- src/main/java/com/polychat/polychatbe/matchingHistory/command/domain/model/MatchingHistory.java
+++ src/main/java/com/polychat/polychatbe/matchingHistory/command/domain/model/MatchingHistory.java
@@ -1,6 +1,7 @@
 package com.polychat.polychatbe.matchingHistory.command.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
@@ -8,21 +9,48 @@ import java.time.LocalDateTime;
 @Table(name = "tbl_matching_history")
 public class MatchingHistory {
 
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int matchingId;
 
+    @Getter
     @Column
-    private long userNumFirst;
+    private Long userNumFoo;
 
+    @Getter
     @Column
-    private long userNumSecond;
+    private Long userNumBar;
 
+    @Getter
     @Column
     private LocalDateTime matchTime;
 
     @Column
     private boolean isAiMatch;
 
+    public MatchingHistory() {
+    }
+
+    public MatchingHistory(Long userNumFoo, Long userNumBar, LocalDateTime matchTime, boolean isAiMatch) {
+        this.userNumFoo = userNumFoo;
+        this.userNumBar = userNumBar;
+        this.matchTime = matchTime;
+        this.isAiMatch = isAiMatch;
+    }
+
+    public boolean isAiMatch() {
+        return isAiMatch;
+    }
+
+    @Override
+    public String toString() {
+        return "MatchingHistory{" +
+                "userNumFirst=" + userNumFoo +
+                ", userNumSecond=" + userNumBar +
+                ", matchTime=" + matchTime +
+                ", isAiMatch=" + isAiMatch +
+                '}';
+    }
 }
