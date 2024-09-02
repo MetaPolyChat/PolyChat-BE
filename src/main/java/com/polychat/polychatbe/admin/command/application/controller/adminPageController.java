a098fb0caf5028c6fc06be09486f6eb70b3ba45e
GarlGaru <juon304@gmail.com>
2024-09-02 21:01:02 +0900

    admin 초안

--- src/main/java/com/polychat/polychatbe/admin/command/application/controller/adminPageController.java
+++ src/main/java/com/polychat/polychatbe/admin/command/application/controller/adminPageController.java
@@ -11,4 +11,26 @@ public class adminPageController {
     public String adminPage() {
         return "Admin Page";
     }
+
+
+    @GetMapping("/admin/blockuser")// block, release, find(count>5)
+    public String adminBlockUser() {
+        return "Admin Block User";
+    }
+
+
+
+    @GetMapping("/admin/logincount") //find, update(count ++)
+    public String adminLoginCount() {
+        return "Admin Login Count";
+    }
+
+    @GetMapping("/admin/question") //find, complete, reply( if(find.moreThanOne) -> update)
+    public String adminQuestion() {
+        return "Admin Question";
+    }
+    @GetMapping("/user/question") //create
+    public String userQuestion() {
+        return "Admin Question";
+    }
 }
