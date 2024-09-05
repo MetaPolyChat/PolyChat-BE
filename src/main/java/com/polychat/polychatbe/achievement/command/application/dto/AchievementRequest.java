46504e5103a10e13878a05ded672b129863903cf
gsh118 <hks01108@naver.com>
2024-09-05 18:17:41 +0900

    #33 swagger 문서 작성
    swagger api에 각 메소드에 대한 설명 추가

--- src/main/java/com/polychat/polychatbe/achievement/command/application/dto/AchievementRequest.java
+++ src/main/java/com/polychat/polychatbe/achievement/command/application/dto/AchievementRequest.java
@@ -1,6 +1,8 @@
 package com.polychat.polychatbe.achievement.command.application.dto;
 
 import com.polychat.polychatbe.achievement.command.domain.aggregate.Achievement;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +14,17 @@ import org.springframework.web.multipart.MultipartFile;
 public class AchievementRequest {
 
     private Long achievementId;
+
+    @NotBlank(message = "업적 이름을 입력해주세요.")
     private String achievementName;
+
+    @NotBlank(message = "업적 설명을 입력해주세요.")
+    @Size(min = 10, message = "업적 설명은 10글자 이상 입력해야 합니다.")
     private String achievementDescription;
+
+    @NotBlank(message = "업적 달성 조건을 입력해주세요.")
     private String achievementCondition;
+
     private MultipartFile achievementImage;
 
     public AchievementRequest(String achievementName, String achievementDescription, String achievementCondition, MultipartFile achievementImage) {
