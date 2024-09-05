897aac1c777305c04c008dc02915418a02597476
gsh118 <hks01108@naver.com>
2024-09-05 18:17:41 +0900

    #33 swagger 문서 작성
    swagger api에 각 메소드에 대한 설명 추가

--- src/main/java/com/polychat/polychatbe/announcement/query/controller/AnnouncementSearchController.java
+++ src/main/java/com/polychat/polychatbe/announcement/query/controller/AnnouncementSearchController.java
@@ -3,6 +3,7 @@ package com.polychat.polychatbe.announcement.query.controller;
 import com.polychat.polychatbe.announcement.query.dto.AnnouncementResponseDTO;
 import com.polychat.polychatbe.announcement.query.service.AnnouncementSearchService;
 import com.polychat.polychatbe.common.SearchCriteriaInfo;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,6 +23,7 @@ public class AnnouncementSearchController {
         this.announcementSearchService = announcementSearchService;
     }
 
+    @Operation(summary = "공지사항 전체 조회", description = "전체 공지사항을 조회합니다.")
     @GetMapping("announcement")
     public ResponseEntity<List<AnnouncementResponseDTO>> getAnnouncementList(@ModelAttribute SearchCriteriaInfo searchCriteriaInfo) {
 
@@ -38,6 +40,7 @@ public class AnnouncementSearchController {
 
     }
 
+    @Operation(summary = "공지사항 조회", description = "지정한 공지사항을 조회합니다.")
     @GetMapping("announcement/{id}")
     public ResponseEntity<AnnouncementResponseDTO> getAnnouncement(@PathVariable long id) {
 
--- src/main/java/com/polychat/polychatbe/friendRequest/command/application/dto/FriendRequestStatusDTO.java
+++ src/main/java/com/polychat/polychatbe/friendRequest/command/application/dto/FriendRequestStatusDTO.java
@@ -1,6 +1,8 @@
 package com.polychat.polychatbe.friendRequest.command.application.dto;
 
 import com.polychat.polychatbe.friendRequest.command.domain.model.RequestStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FriendRequestStatusDTO {
+
+    @NotBlank(message = "친구 신청 정보를 확인해주세요.")
+    @Positive
     private Integer friendRequestId;
+
+    @NotBlank(message = "친구 신청 상태를 확인해주세요.")
     private RequestStatus status;
 
 }
