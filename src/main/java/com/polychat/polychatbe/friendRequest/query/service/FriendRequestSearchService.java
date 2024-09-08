bc1b3200d36926095b10ac2cff00751b5c2ab13a
gsh118 <hks01108@naver.com>
2024-09-08 17:54:41 +0900

    #36 친구 신청 기능 리팩토링
    친구 신청 조회용 repository를 mybatis 방식으로 변경

--- src/main/java/com/polychat/polychatbe/friendRequest/query/service/FriendRequestSearchService.java
+++ src/main/java/com/polychat/polychatbe/friendRequest/query/service/FriendRequestSearchService.java
@@ -1,12 +1,12 @@
 package com.polychat.polychatbe.friendRequest.query.service;
 
 //import com.polychat.polychatbe.friend.command.domain.model.Friend;
-import com.polychat.polychatbe.friendRequest.command.domain.model.FriendRequest;
 import com.polychat.polychatbe.friendRequest.query.dto.FriendRequestInfoDTO;
+import com.polychat.polychatbe.friendRequest.query.dto.UserFriendRequestDTO;
 import com.polychat.polychatbe.friendRequest.query.repository.FriendRequestSearchRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.NoSuchElementException;
 
 @Service
 public class FriendRequestSearchService {
@@ -18,30 +18,19 @@ public class FriendRequestSearchService {
     }
 
     public List<FriendRequestInfoDTO> findAllFriendRequest() {
-        return friendRequestSearchRepository.findAll()
-                .stream()
-                .map((request)-> new FriendRequestInfoDTO(
-                        request.getFriendRequestId(),
-                        request.getSender(),
-                        request.getReceiver(),
-                        request.getStatus()))
-                .collect(Collectors.toList());
+        return friendRequestSearchRepository.findAllFriendRequest();
     }
 
-    public List<FriendRequestInfoDTO> findFriendRequestBySender(int sender) {
-        return friendRequestSearchRepository.findBySender(sender)
-                .stream()
-                .map(FriendRequestInfoDTO::new)
-                .collect(Collectors.toList());
+    public List<UserFriendRequestDTO> findFriendRequestBySender(int sender) {
+        return friendRequestSearchRepository.findFriendRequestBySender(sender);
     }
 
     public FriendRequestInfoDTO findFriendRequestById(long id) {
 
-        FriendRequest request = friendRequestSearchRepository.findById(id).orElseThrow(
-                ()->new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
-        );
+        FriendRequestInfoDTO friendRequestInfo = friendRequestSearchRepository.findFriendRequestById(id)
+                .orElseThrow(() -> new NoSuchElementException("해당 아이디가 존재하지 않습니다"));
 
-        return new FriendRequestInfoDTO(request);
+        return friendRequestInfo;
     }
 
 }
