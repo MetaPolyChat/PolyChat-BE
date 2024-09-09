389d4366102447b09e2259a39d2178b6beb5864d
gsh118 <hks01108@naver.com>
2024-09-09 23:50:24 +0900

    userAchievement 엔티티 추가

---
+++ src/main/java/com/polychat/polychatbe/userAchievement/command/domain/repository/UserAchievementRepository.java
@@ -0,0 +1,9 @@
+package com.polychat.polychatbe.userAchievement.command.domain.repository;
+
+
+import com.polychat.polychatbe.userAchievement.command.domain.aggregate.UserAchievement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAchievementRepository extends JpaRepository<UserAchievement, Long> {
+
+}
