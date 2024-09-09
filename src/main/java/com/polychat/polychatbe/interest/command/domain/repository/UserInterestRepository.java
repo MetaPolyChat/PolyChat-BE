329af1d266887359d2c46947aa38f09ce141a126
gsh118 <hks01108@naver.com>
2024-09-09 23:36:03 +0900

    merge

--- src/main/java/com/polychat/polychatbe/interest/command/domain/repository/UserInterestRepository.java
+++ src/main/java/com/polychat/polychatbe/interest/command/domain/repository/UserInterestRepository.java
@@ -1,5 +1,5 @@
-package com.polychat.polychatbe.inerest.command.domain.repository;
-import com.polychat.polychatbe.inerest.command.domain.model.UserInterest;
+package com.polychat.polychatbe.interest.command.domain.repository;
+import com.polychat.polychatbe.interest.command.domain.model.UserInterest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +8,5 @@ import java.util.List;
 @Repository
 public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
     UserInterest findByUserNoAndInterestNo(Long userNo, Long interestNo);
-    List<UserInterest> findByInterestNo(Long interestNo);
+    List<UserInterest> findAllByInterestNo(Long interestNo);
 }
