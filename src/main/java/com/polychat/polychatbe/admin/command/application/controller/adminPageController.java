5b0aee91b1f57e2e220dae742f754aa98bd94c70
GarlGaru <juon304@gmail.com>
2024-09-09 18:32:34 +0900

    admin/block swagger 업데이트

--- src/main/java/com/polychat/polychatbe/admin/command/application/controller/adminPageController.java
+++ src/main/java/com/polychat/polychatbe/admin/command/application/controller/adminPageController.java
@@ -2,6 +2,9 @@ package com.polychat.polychatbe.admin.command.application.controller;
 
 import com.polychat.polychatbe.blockList.command.application.service.BlockListAppService;
 import com.polychat.polychatbe.blockList.command.domain.model.BlockList;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,26 +28,33 @@ public class adminPageController {
         return "adminDashboard";
     }
 
-
-    @GetMapping(value = "/admin/blockuser", produces = "application/json; charset=UTF-8")// block, release, find(count>5)
+    // block, release, find(count>5)
+    @Operation(summary = "전체 블록 리스트", description = "블록 리스트 전체를 불러옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "블록리스트를 모두 가져왔습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 에러")
+    })
+    @GetMapping(value = "/admin/blockuser", produces = "application/json; charset=UTF-8")
     public ResponseEntity adminBlockUser() {
         List<BlockList> blockLists = blockListAppService.getAllBlockLists();
         System.out.println(blockLists);
         return ResponseEntity.ok(blockLists);
     }
 
-
-    @GetMapping("/admin/logincount") //find, update(count ++)
+    //find, update(count ++)
+    @GetMapping("/admin/logincount")
     public String adminLoginCount() {
         return "Admin Login Count";
     }
 
-    @GetMapping("/admin/question") //find, complete, reply( if(find.moreThanOne) -> update)
+    //find, complete, reply( if(find.moreThanOne) -> update)
+    @GetMapping("/admin/question")
     public String adminQuestion() {
         return "Admin Question";
     }
 
-    @GetMapping("/user/question") //create
+    //create
+    @GetMapping("/user/question")
     public String userQuestion() {
         return "Admin Question";
     }
