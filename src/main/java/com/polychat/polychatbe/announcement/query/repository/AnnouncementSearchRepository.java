fe1752f5353f4fc79c8acf118e94c2d8f44e097c
gsh118 <hks01108@naver.com>
2024-09-18 01:17:38 +0900

    공지사항 조회 부분 페이지네이션 코드 추가

--- src/main/java/com/polychat/polychatbe/announcement/query/repository/AnnouncementSearchRepository.java
+++ src/main/java/com/polychat/polychatbe/announcement/query/repository/AnnouncementSearchRepository.java
@@ -1,14 +1,19 @@
 package com.polychat.polychatbe.announcement.query.repository;
 
 import com.polychat.polychatbe.announcement.query.dto.AnnouncementResponseDTO;
+import com.polychat.polychatbe.common.SearchCriteriaInfo;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface AnnouncementSearchRepository {
 
     List<AnnouncementResponseDTO> findAllAnnouncement();
 
-    AnnouncementResponseDTO findAnnouncementById(Long announcementId);
+    List<AnnouncementResponseDTO> findAllAnnouncement(SearchCriteriaInfo searchCriteriaInfo);
+
+    Optional<AnnouncementResponseDTO> findAnnouncementById(Long announcementId);
+
 }
--- src/main/java/com/polychat/polychatbe/common/SearchCriteriaInfo.java
+++ src/main/java/com/polychat/polychatbe/common/SearchCriteriaInfo.java
@@ -1,23 +1,42 @@
 package com.polychat.polychatbe.common;
 
-
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class SearchCriteriaInfo {
-    private String searchCriteria;
-    private String searchValue;
-    private Integer offset;
-    private Integer limit;
-    private List<String> filter;
-    private String orderCriteria;
-    private OrderMethod orderMethod;
+    private final String searchCriteria;
+    private final String searchValue;
+    private final List<String> filter;
+    private final String orderCriteria;
+    private final OrderMethod orderMethod;
+
+    @Positive(message = "페이지 번호는 0보다 커야합니다.")
+    private final Integer pageNum;
+    @Positive(message = "표시할 갯수는 0보다 커야합니다.")
+    private final Integer limit;
+    private final Integer offset;
+
+    public SearchCriteriaInfo(String searchCriteria, String searchValue, List<String> filter,
+                              String orderCriteria, OrderMethod orderMethod,
+                              Integer pageNum, Integer limit) {
+        this.searchCriteria = searchCriteria;
+        this.searchValue = searchValue;
+        this.filter = filter;
+        this.orderCriteria = orderCriteria;
+        this.orderMethod = orderMethod;
+
+        this.pageNum = pageNum != null ? pageNum : 1;
+        this.limit = limit != null ? limit : 3;
+
+        this.offset = (this.pageNum - 1) * this.limit;
+//        if(this.pageNum !=null && this.limit !=null) {
+//            this.offset = (this.pageNum-1) * this.limit;
+//        } else {
+//            this.offset=null;
+//        }
+    }
 
 }
