3646746011ee08fcba0ef4502f03810036600cad
syamcat <kdh1998kdh@gmail.com>
2024-09-23 17:32:54 +0900

    feature UserLogin & fix UserSocialLogin Bean

--- src/main/java/com/polychat/polychatbe/user/command/domain/repository/UserRepository.java
+++ src/main/java/com/polychat/polychatbe/user/command/domain/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findUserByEmail(String email);
+    Optional<User> findUserByEmailAndStatus(String email, String status);
 
-    Optional<User> findUserByUserId(String userId);
+    Optional<User> findByEmail(String email);
 }
