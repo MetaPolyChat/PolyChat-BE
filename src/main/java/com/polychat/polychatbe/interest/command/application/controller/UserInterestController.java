6ce84c945fdddb164f20ba6a73dec75b9220a349
syamcat <kdh1998kdh@gmail.com>
2024-09-24 11:01:07 +0900

    commant error

--- src/main/java/com/polychat/polychatbe/interest/command/application/controller/UserInterestController.java
+++ src/main/java/com/polychat/polychatbe/interest/command/application/controller/UserInterestController.java
@@ -1,14 +1,13 @@
 package com.polychat.polychatbe.interest.command.application.controller;
 
+import com.polychat.polychatbe.interest.command.application.dto.InterestDTO;
 import com.polychat.polychatbe.interest.command.application.dto.SignUpUserInterestDTO;
 import com.polychat.polychatbe.interest.command.application.dto.UserInterestDTO;
 import com.polychat.polychatbe.interest.command.domain.service.InterestService;
 import com.polychat.polychatbe.interest.command.domain.service.UserInterestService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,7 @@ public class UserInterestController {
         this.userInterestService = userInterestService;
     }
 
+    @Operation(summary = "관심사 경로", description = "관심사 등록 api")
     @PostMapping("/regist")
     public ResponseEntity<?> registerInterest(@RequestBody SignUpUserInterestDTO interest) {
 
@@ -34,4 +34,13 @@ public class UserInterestController {
 //        return ResponseEntity.ok(result);
         return ResponseEntity.ok().build();
     }
+
+//    @Operation(summary = "관심사 수정", description = "관심사 수정 api")
+//    @PutMapping("/update")
+//    public ResponseEntity<?> updateInterest(@RequestBody InterestDTO interest) {
+//
+//    }
+//
+//    @GetMapping("/find-user")
+//    public
 }
