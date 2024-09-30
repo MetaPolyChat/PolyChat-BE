5fbcb80051757e281d1d0df0799a3d3c0d5c780c
gsh118 <hks01108@naver.com>
2024-09-30 11:29:38 +0900

    친구 요청 수락시 이벤트 방식으로 친구 등록되도록 변경

--- src/main/java/com/polychat/polychatbe/friend/command/domain/service/FriendRequestAcceptService.java
+++ src/main/java/com/polychat/polychatbe/friend/command/domain/service/FriendRequestAcceptService.java
@@ -1,35 +1,22 @@
 package com.polychat.polychatbe.friend.command.domain.service;
 
-import com.polychat.polychatbe.friend.command.domain.repository.FriendRepository;
-import com.polychat.polychatbe.friendRequest.command.domain.model.FriendRequest;
-import com.polychat.polychatbe.friendRequest.command.domain.model.RequestStatus;
-import jakarta.transaction.Transactional;
+import com.polychat.polychatbe.friendRequest.command.domain.service.FriendRequestApprovedEvent;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
 public class FriendRequestAcceptService {
 
-    private FriendRepository friendRepository;
     private FriendService friendService;
 
-    public FriendRequestAcceptService(FriendRepository friendRepository, FriendService friendService) {
-        this.friendRepository = friendRepository;
+    public FriendRequestAcceptService(FriendService friendService) {
         this.friendService = friendService;
     }
 
-    @Transactional
-    public void acceptFriendRequest(FriendRequest friendRequest) {
-
-        if (friendRequest.getStatus() != RequestStatus.ACCEPTED) {
-            throw new IllegalArgumentException("승인되지 않은 친구 신청입니다.");
-        }
-
-        int user1=friendRequest.getSender();
-        int user2=friendRequest.getReceiver();
-
-        friendService.addFriend(user1, user2);
-        friendService.addFriend(user2, user1);
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleFriendRequestApproved(FriendRequestApprovedEvent event) {
+        friendService.addFriend(event.getReceiverId(), event.getSenderId());
     }
-
 
 }
