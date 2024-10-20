cb195f5b92b57005082397a8ceb5a154131a6596
gsh118 <hks01108@naver.com>
2024-10-20 23:57:24 +0900

    POL-22 아이템 도메인 api 작성

--- src/main/java/com/polychat/polychatbe/item/query/repository/ItemSearchRepository.java
+++ src/main/java/com/polychat/polychatbe/item/query/repository/ItemSearchRepository.java
@@ -1,5 +1,6 @@
 package com.polychat.polychatbe.item.query.repository;
 
+import com.polychat.polychatbe.common.SearchCriteriaInfo;
 import com.polychat.polychatbe.item.query.dto.ItemDetailResponseDTO;
 import com.polychat.polychatbe.item.query.dto.ItemResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
@@ -12,6 +13,7 @@ public interface ItemSearchRepository {
 
     Optional<ItemDetailResponseDTO> findById(long itemId);
     List<ItemResponseDTO> findAllItem();
+    List<ItemResponseDTO> findItems(SearchCriteriaInfo searchCriteria);
     int countAll();
 
 }
