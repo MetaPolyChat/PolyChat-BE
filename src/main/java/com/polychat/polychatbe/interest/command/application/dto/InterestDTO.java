a05cf2993b4681d3eed1ecc91eac5bafb83dff8a
syamcat <kdh1998kdh@gmail.com>
2024-10-23 20:12:48 +0900

    add find all interests api

--- src/main/java/com/polychat/polychatbe/interest/command/application/dto/InterestDTO.java
+++ src/main/java/com/polychat/polychatbe/interest/command/application/dto/InterestDTO.java
@@ -1,38 +1,14 @@
 package com.polychat.polychatbe.interest.command.application.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
 public class InterestDTO {
 
-    private Long interestNo;
+    private Long interestId;
+
     private String interestName;
-
-    public InterestDTO() {}
-
-    public InterestDTO(Long interestNo, String interestName) {
-        this.interestNo = interestNo;
-        this.interestName = interestName;
-    }
-
-    public Long getInterestNo() {
-        return interestNo;
-    }
-
-    public void setInterestNo(Long interestNo) {
-        this.interestNo = interestNo;
-    }
-
-    public String getInterestName() {
-        return interestName;
-    }
-
-    public void setInterestName(String interestName) {
-        this.interestName = interestName;
-    }
-
-    @Override
-    public String toString() {
-        return "InterestDTO{" +
-                "interestNo=" + interestNo +
-                ", interestName='" + interestName + '\'' +
-                '}';
-    }
 }
