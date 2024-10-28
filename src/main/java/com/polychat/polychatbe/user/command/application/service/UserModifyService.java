6b78cf9d56c752b4bea372e449c0ea158e7f8ed9
gsh118 <hks01108@naver.com>
2024-10-29 08:28:22 +0900

    유저 서비스 코드 및 일부 코드 수정
    유저 application service에서 user정보 못찾을 시 에러나도록 하는 부분 모듈화

--- src/main/java/com/polychat/polychatbe/user/command/application/service/UserModifyService.java
+++ src/main/java/com/polychat/polychatbe/user/command/application/service/UserModifyService.java
@@ -1,5 +1,7 @@
 package com.polychat.polychatbe.user.command.application.service;
 
+import com.polychat.polychatbe.common.error.ApplicationException;
+import com.polychat.polychatbe.common.error.ErrorCode;
 import com.polychat.polychatbe.user.command.application.dto.UserModifyDTO;
 import com.polychat.polychatbe.user.command.domain.model.Authority;
 import com.polychat.polychatbe.user.command.domain.model.Status;
@@ -10,8 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.NoSuchElementException;
-
 @Service
 @RequiredArgsConstructor
 public class UserModifyService {
@@ -19,12 +19,16 @@ public class UserModifyService {
     private final UserRepository userRepository;
     private final AuthorityCheckService authorityCheckService;
 
+    public User findByUserId(long userId){
+        return  userRepository.findById(userId).orElseThrow(
+                ()-> new ApplicationException(ErrorCode.NOT_EXIST_MEMBER)
+        );
+    }
+
     @Transactional
     public void modifyUserAccountStatus(long changedUserId, UserModifyDTO userModifyInfo){
 
-        User user = userRepository.findById(changedUserId).orElseThrow(
-                ()-> new NoSuchElementException("해당 유저를 찾을 수 없습니다.")
-        );
+        User user = findByUserId(changedUserId);
 
         if (authorityCheckService.hasAccountModify(userModifyInfo.getModifierId())){
             throw new IllegalArgumentException("계정 정보를 수정할 권한이 존재하지 않습니다.");
@@ -40,9 +44,7 @@ public class UserModifyService {
     }
 
     public void withdraw(long userId) {
-        User user = userRepository.findById(userId).orElseThrow(
-                ()-> new NoSuchElementException("해당 유저를 찾을 수 없습니다.")
-        );
+        User user = findByUserId(userId);
 
         if(user.getStatus() == Status.DEACTIVATED){
             throw new IllegalStateException("이미 탈퇴된 회원입니다.");
@@ -66,12 +68,8 @@ public class UserModifyService {
             throw new IllegalArgumentException("계정 정보를 수정할 권한이 존재하지 않습니다.");
         }
 
-        User user = userRepository.findById(userId).orElseThrow(
-                ()-> new NoSuchElementException("해당 유저를 찾을 수 없습니다.")
-        );
+        User user = findByUserId(userId);
 
         user.updateAuthority(Authority.USER);
-
-
     }
 }
