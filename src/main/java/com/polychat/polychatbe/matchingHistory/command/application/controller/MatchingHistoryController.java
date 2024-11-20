fe2bef451dcd0e9114652500b12e00af424bee5a
GarlGaru <juon304@gmail.com>
2024-11-20 15:37:51 +0900

    refactor : 매칭 히스토리 createdAt 추가, 컨트롤러 완성, mapstruct 추가

--- src/main/java/com/polychat/polychatbe/matchingHistory/command/application/controller/MatchingHistoryController.java
+++ src/main/java/com/polychat/polychatbe/matchingHistory/command/application/controller/MatchingHistoryController.java
@@ -1,38 +1,69 @@
 package com.polychat.polychatbe.matchingHistory.command.application.controller;
 
+import com.polychat.polychatbe.matchingHistory.command.application.dto.CreateMatchingHistoryDTO;
 import com.polychat.polychatbe.matchingHistory.command.application.dto.MatchingHistoryDTO;
+import com.polychat.polychatbe.matchingHistory.command.application.dto.ReadMatchingHistoryDTO;
 import com.polychat.polychatbe.matchingHistory.command.application.service.MatchingHistoryAppService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController()
+@RequiredArgsConstructor
+@RequestMapping("/history")
+@RestController
+@Tag(name = "매칭 히스토리", description = "유저들끼리나 ai 상대로 채팅 매칭된 기록")
 public class MatchingHistoryController {
 
-    private final MatchingHistoryAppService matchingHistoryAppService;
+    private final MatchingHistoryAppService appService;
 
-    public MatchingHistoryController(MatchingHistoryAppService matchingHistoryAppService) {
-        this.matchingHistoryAppService = matchingHistoryAppService;
+    @Operation(summary = "모든 기록", description = "모든 기록을 불러옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 처리 되었습니다."),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
+    })
+    @GetMapping("/list")
+    public ResponseEntity<?> findAllList() {
+        List<MatchingHistoryDTO> result = appService.findAllHistory();
+        return ResponseEntity.ok(result);
     }
 
-//    @GetMapping(value = "/user", produces = "application/json; charset=UTF-8")
-//    @ResponseBody()
-//    public List<MatchingHistoryDTO> getMatchingHistoryByUserIdToJson(@RequestParam String userId) {
-//        return matchingHistoryAppService.findMatchingHistory(Long.parseLong(userId));
-//    }
+    @Operation(summary = "유저 기록", description = "유저의 모든 기록을 불러옵니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 처리 되었습니다."),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
+    })
+    @GetMapping("/user")
+    public ResponseEntity<?>  findHistory(@RequestParam String userId) {
+        List<ReadMatchingHistoryDTO> result = appService.findMatchingHistory(Long.parseLong(userId));
+        return ResponseEntity.ok(result);
+    }
 
-    @GetMapping(value = "/history/user", produces = "application/json; charset=UTF-8")
-    public ResponseEntity getMatchingHistoryByUserIdToJson(@RequestParam String userId) {
-        List<MatchingHistoryDTO> result = matchingHistoryAppService.findMatchingHistory(Long.parseLong(userId));
-        if (result.size() > 0) {
-            return ResponseEntity.ok().body(result);
-        } else {
-            return ResponseEntity.status(400).body("{\"message\" : \"조회실패\"}");
-        }
+    @Operation(summary = "기록 입력", description = "매칭 기록을 입력합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 처리 되었습니다."),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
+    })
+    @PostMapping("/create")
+    public ResponseEntity<?> createHistory(@RequestBody CreateMatchingHistoryDTO createDto) {
+        MatchingHistoryDTO created = appService.createNewMatchingHistory(createDto);
+        return ResponseEntity.ok(created);
+    }
+
+    @Operation(summary = "기록 입력", description = "매칭 기록을 입력합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 처리 되었습니다."),
+            @ApiResponse(responseCode = "500", description = "예상치 못한 예러")
+    })
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteHistory(@RequestParam Long historyId) {
+        appService.deleteMatchingHistory(historyId);
+        return ResponseEntity.ok().build();
     }
 
 
