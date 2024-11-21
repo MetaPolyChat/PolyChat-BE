e5cbadd141d7258605876f4b350983714e4bd79d
GarlGaru <juon304@gmail.com>
2024-11-21 15:00:42 +0900

    유저 정보 컨트롤러 추가

--- src/main/java/com/polychat/polychatbe/user/command/application/controller/UserApiController.java
+++ src/main/java/com/polychat/polychatbe/user/command/application/controller/UserApiController.java
@@ -1,11 +1,12 @@
 package com.polychat.polychatbe.user.command.application.controller;
 
+import com.polychat.polychatbe.user.command.application.dto.UserRequestDTO;
 import com.polychat.polychatbe.user.command.application.dto.UserResponseDTO;
 import com.polychat.polychatbe.user.command.application.service.UserInfoService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController("/user")
@@ -16,31 +17,59 @@ public class UserApiController {
     /**
      * 시스템용 정보 호출
      */
+    @Operation(summary = "시스템용 유저 info")
     @GetMapping("/info")
-    public ResponseEntity<?> findUserInfo(Long userId) {
+    public ResponseEntity<?> findUserInfo(@RequestParam Long userId) {
         UserResponseDTO.UserInfoDTO response = userService.findUserById(userId);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/nickName")
-    public ResponseEntity<?> findUserNickName(Long userId) {
-        UserResponseDTO.nickNameDTO response = userService.findUserName(userId);
         return ResponseEntity.ok(response);
     }
 
     /**
      * 내 프로필 조회 (interest 포함)
      */
+    @Operation(summary = "내 프로필 (관심사 포함)")
+    @GetMapping("/my-profile")
+    public ResponseEntity<?> getMyProfile(@RequestParam Long userId) {
+        UserResponseDTO.UserMyProfileDTO profileDTO = userService.getMyProfile(userId);
+        return ResponseEntity.ok(profileDTO);
+    }
 
 
     /**
      * 다른 사람 프로필 조회? (interest 포함)
      */
+    @Operation(summary = "다른 유저 프로필 (관심사 포함)")
+    @GetMapping("/profile")
+    public ResponseEntity<?> findUserProfile(@RequestParam String planet) {
+        UserResponseDTO.UserProfileDTO profileDTO = userService.findUserProfile(planet);
+        return ResponseEntity.ok(profileDTO);
+    }
 
 
     /**
      * 간단 조회
      */
+    @Operation(summary = "유저 닉네임 조회")
+    @GetMapping("/nickName")
+    public ResponseEntity<?> findUserNickName(@RequestParam Long userId) {
+        UserResponseDTO.nickNameDTO response = userService.findUserName(userId);
+        return ResponseEntity.ok(response);
+    }
 
+    @Operation(summary = "유저 존재 여부")
+    @GetMapping("/exist")
+    public ResponseEntity<?> findUserExist(@RequestParam Long userId) {
+        return ResponseEntity.ok(userService.findUserExist(userId));
+    }
+
+    /**
+     * 회원 정보 수정
+     * */
+    @Operation(summary = "유저 닉네임 변경")
+    @PutMapping("/update/nickname")
+    public ResponseEntity<?> updateUserNickname(@RequestBody UserRequestDTO.UpdateNameDTO updateNameDTO) {
+        userService.updateUserNickname(updateNameDTO);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/com/polychat/polychatbe/user/command/application/service/UserInfoService.java
+++ src/main/java/com/polychat/polychatbe/user/command/application/service/UserInfoService.java
@@ -2,6 +2,10 @@ package com.polychat.polychatbe.user.command.application.service;
 
 import com.polychat.polychatbe.common.error.ApplicationException;
 import com.polychat.polychatbe.common.error.ErrorCode;
+import com.polychat.polychatbe.interest.command.application.service.InterestUserService;
+import com.polychat.polychatbe.user.command.application.dto.UserDTO;
+import com.polychat.polychatbe.user.command.application.dto.UserInfoMapper;
+import com.polychat.polychatbe.user.command.application.dto.UserRequestDTO;
 import com.polychat.polychatbe.user.command.application.dto.UserResponseDTO;
 import com.polychat.polychatbe.user.command.domain.model.Authority;
 import com.polychat.polychatbe.user.command.domain.model.User;
@@ -10,46 +14,78 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional(readOnly = true)
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class UserInfoService {
 
-    public final UserService userService;
+    private final UserService userService;
+    private final UserInfoMapper mapper;
+
+    private final InterestUserService interestService;
+
+    public Boolean findUserExist(Long id) {
+        return userService.findExistUserById(id);
+    }
+
+    private User findUserByIdOrThrow(Long id){
+        User user = userService.findUserById(id);
+        if (user == null) {
+            throw new ApplicationException(ErrorCode.NO_SUCH_USER);
+        }
+        return user;
+    }
 
 
+
+    @Transactional(readOnly = true)
     public UserResponseDTO.UserInfoDTO findUserById(Long id) {
-        User user = userService.findUserById(id);
-        if (user == null) {
-            throw new ApplicationException(ErrorCode.NO_SUCH_USER);
-        }
+        User user = this.findUserByIdOrThrow(id);
 
         if (Authority.ADMIN.equals(user.getAuthority())){
             throw new ApplicationException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
 
-        return new UserResponseDTO.UserInfoDTO(
-                user.getUserId(),
-                user.getEmail(),
-                user.getUserName(),
-                user.getLoginType(),
-                user.getPlanet()
-        );
+        return mapper.toInfoDto(user);
     }
 
+    @Transactional(readOnly = true)
     public UserResponseDTO.nickNameDTO findUserName(Long id) {
-        User user = userService.findUserById(id);
-        if (user == null) {
-            throw new ApplicationException(ErrorCode.NO_SUCH_USER);
-        }
+        User user = this.findUserByIdOrThrow(id);
 
         if (Authority.ADMIN.equals(user.getAuthority())){
             throw new ApplicationException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
 
-        return new UserResponseDTO.nickNameDTO(
-                user.getUserName()
-        );
+        return new UserResponseDTO.nickNameDTO(user.getUserName());
     }
 
+    public UserResponseDTO.UserMyProfileDTO getMyProfile(Long userId) {
+        User user = this.findUserByIdOrThrow(userId);
+        List<String> interests = interestService.findInterestNamesByUserId(userId);
+        return mapper.toMyProfileDto(user, interests);
+    }
+
+    public UserResponseDTO.UserProfileDTO findUserProfile(String planet) {
+        User user = userService.findUserByPlanet(planet);
+        if (user == null) {
+            throw new ApplicationException(ErrorCode.NO_SUCH_USER);
+        }
+        List<String> interests = interestService.findInterestNamesByUserId(user.getUserId());
+        return mapper.toProfileDto(user, interests);
+    }
+    public UserResponseDTO.UserProfileDTO findUserProfile(Long userId) {
+        User user = this.findUserByIdOrThrow(userId);
+        List<String> interests = interestService.findInterestNamesByUserId(userId);
+        return mapper.toProfileDto(user, interests);
+    }
+
+    public void updateUserNickname(UserRequestDTO.UpdateNameDTO updateNameDTO) {
+        User user = this.findUserByIdOrThrow(updateNameDTO.userId());
+        UserDTO userDTO = mapper.toDto(user);
+        userDTO.setUserName(updateNameDTO.userName());
+        user = mapper.toEntity(userDTO);
+        userService.updateUser(user);
+    }
 }
